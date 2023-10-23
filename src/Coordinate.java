17f0d4862413c375d4c94a5b7da82bec3c29ffb7
Pantarelo <118564862+Pantarelo@users.noreply.github.com>
2023-10-23 23:34:56 +0300

    Lee

---
+++ src/Coordinate.java
@@ -0,0 +1,11 @@
+class Coordinate {
+    int i;
+    int j;
+    int length;
+
+    public Coordinate(int i, int j, int length) {
+        this.i = i;
+        this.j = j;
+        this.length = length;
+    }
+}
